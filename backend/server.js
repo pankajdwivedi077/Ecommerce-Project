0e0054d7933011cbf6911207c31ee39ff982930d
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2025-01-23 16:15:38 +0530

    start

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose.connect('mongodb+srv://pankaj:Tag0X4Jir3nLHHii@cluster0.x0oak.mongodb.net/').then(() => {
+  console.log("mongodb connected")
+}).catch((error) => console.log(error))
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+        allowedHeaders: [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`)
+})
