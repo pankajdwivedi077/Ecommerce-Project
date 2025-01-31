2f21853387d344ec7802ef44e92137b84b57c244
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2025-01-31 17:09:29 +0530

    form

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const authRouter = require('./routes/auth/auth-routes')
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log("mongodb connected")
@@ -28,6 +29,7 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,8 +1,35 @@
+import CommonForm from "@/components/common/Form";
+import { loginFormControls } from "@/config";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: '',
+  password: ''
+}
 
 function AuthLogin(){
-  return (
-    <div>Login</div>
-  )
+  const [formData, setFormData] = useState(initialState)
+
+  function onSubmit() {
+
+  }
+
+    return <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground" >Sign in to your account</h1>
+        <p className="mt-2">Don't have an account
+          <Link className="font-medium ml-2 text-primary hover:underline" to='/auth/register' >Register</Link>
+        </p>
+      </div>
+      <CommonForm 
+       formContrls={loginFormControls}
+       buttonText={'Sign In'}
+       formData={formData}
+       setFormData={setFormData}
+       onSubmit={onSubmit}
+      />
+    </div>
 }
 
 export default AuthLogin;
