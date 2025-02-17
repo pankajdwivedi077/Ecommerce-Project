f1550a2232745e78d45ae4bc90a384b867702ba4
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2025-02-17 15:21:21 +0530

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // 👈 Important for correct asset loading on Render
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
